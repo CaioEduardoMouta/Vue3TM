c15db6621bc8b0d141329cfefe88d77ac115ad81
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-10-02 18:26:27 -0300

    vue

---
+++ projetovue/projetoVUE/src/App.vue
@@ -0,0 +1,50 @@
+
+
+<template>
+  <!-- Content -->
+  <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+
+         
+            <TodoSpinner />
+            <TodoFormAdd />
+            
+            <TodoItems/>
+            <TodoEmpty/>
+            
+        
+            
+        </div>
+    </div>
+    <!--/ Content -->
+</template>
+
+<script>
+import TodoSpinner from './components/TodoSpinner.vue';
+import TodoFormAdd from './components/TodoFormAdd.vue';
+import TodoItems from './components/TodoItems.vue';
+import TodoEmpty from './components/TodoEmpty.vue';
+
+
+export default {
+  
+  components: {
+    TodoSpinner,
+    TodoFormAdd,
+    TodoItems,
+    TodoEmpty
+    
+},''esxfj
+
+created() {
+  axios.get('http://localhost:3000/todos')
+        .then((response)  => {
+          console.log(response.data);
+        })
+}, 
+}
+</script>
+
+<style>
+
+</style>
---
+++ projetovue/projetoVUE/src/components/TodoItems.vue
@@ -0,0 +1,15 @@
+<template>
+     <!-- Todo items -->
+    <div class="space-y-2">
+       <TodoItem/>
+       <TodoItem/>
+ 
+    </div>
+</template>
+
+<script>
+import TodoItem from '@components/TodoItem';    
+export default {
+    components: { TodoItem }
+}
+</script>
